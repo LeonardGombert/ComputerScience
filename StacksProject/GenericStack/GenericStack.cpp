158d6dfafb67dbd7c3976abd9ef948ced9aed343
Léonard <epoc4oftime@gmail.com>
2021-03-24 14:24:19 +0100

    Functional Circular Array for Queue implementation

--- StacksProject/GenericStack/GenericStack.cpp
+++ StacksProject/GenericStack/GenericStack.cpp
@@ -4,18 +4,6 @@
 
 int main()
 {
-	Stack<int> stack;
-	stack.push(new int(4));
-	stack.push(new int(5));
-	stack.push(new int(6));
-	stack.push(new int(7));
-
-	printf("The top-most item of the stack is %d.\n", stack.peek());
-	stack.pop();
-	stack.pop();
-	printf("After popping twice : %d.\n", stack.peek());
-
-
 	Queue<int> queue;
 	queue.enqueue(new int(4));
 	queue.enqueue(new int(5));
@@ -25,14 +13,9 @@ int main()
 	queue.enqueue(new int(9));
 	queue.enqueue(new int(10));
 
-	printf("The oldest-item of the queue is %d.\n", queue.peek());
-
 	queue.dequeue();
 	queue.dequeue();
 	queue.dequeue();
-	queue.dequeue();
-	queue.dequeue();
-
 	printf("The oldest-item of the queue after three dequeues is %d.\n", queue.peek());
 
 	return 0;
--- StacksProject/GenericStack/Queue.h
+++ StacksProject/GenericStack/Queue.h
@@ -1,12 +1,23 @@
 #pragma once
 template<typename T>
 class Queue {
-	const int queueSize = 100;
+	const int queueSize = 4; // the circular array is of size 4
 	T* queue = new T[queueSize];
 	int front = 0, back = 0;
 public:
-	void checkCircle() { if (back >= queueSize) back = 0; if (front >= queueSize) front = 0; } // make the array go full circle (needs fixing)
+	void checkCircle(); // make the array go full circle (needs fixing)
 	void enqueue(T* item) { checkCircle(); queue[back++] = *(item); }
 	void dequeue() { front++; }
 	T& peek() const { return queue[front]; }
 };// queue implementation should use a circular array to avoid having to shift everything "down"
+
+template<typename T>
+inline void Queue<T>::checkCircle()
+{
+	if (back > queueSize)
+	{
+		back = 0;		
+		printf("The queue was full.\n");
+	}
+	if (front > queueSize) front = 0;
+}
